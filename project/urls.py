0c1386e00da1b704ae43a38f8520c5eb39e96fcc
Roman Mataev <roman.mataev@gmail.com>
2019-12-21 21:59:09 +0000

    22122019 добавил свод участников, детализацию проекта и участника

--- project/urls.py
+++ project/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from . views import summary_view
+from . views import summary_project_view, ProjectDetailView
+from . views import summary_member_view, MemberDetailView
 
 app_name = 'project'
 
 urlpatterns = [
 
-    path('', summary_view, name='summary_page'),
-
+    path('', summary_project_view, name='summary_page'),
+    path('<int:pk>/', ProjectDetailView.as_view(), name='detail'),
+    path('member/', summary_member_view, name='summary_member_page'),
+    path('member/<int:pk>/', MemberDetailView.as_view(), name='member'),
 ]
--- project/views.py
+++ project/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
-from .models import Project
+from .models import Project, Member
+from django.views.generic.detail import DetailView
 
+def summary_project_view(request):
+    return render(request, 'summary_project.html', {'project': Project.objects.all()})
 
-def summary_view(request):
-    return render(request, 'summary.html', {'project': Project.objects.all()})
+class ProjectDetailView(DetailView):
+    queryset = Project.objects.all()
+    context_object_name = 'object'
+    template_name = 'project.html'
+
+def summary_member_view(request):
+    return render(request, 'summary_member.html', {'member': Member.objects.all()})
+
+class MemberDetailView(DetailView):
+    queryset = Member.objects.all()
+    context_object_name = 'member'
+    template_name = 'member.html'
+
